9844472977ae354c5b19f9f1ec42ffd09b6a4428
nervi0s <nervi0s@protonmail.com>
2020-10-31 00:30:15 +0100

    Add button listener

--- app/src/main/java/com/damnervi/practice_02_calculator/MainActivity.java
+++ app/src/main/java/com/damnervi/practice_02_calculator/MainActivity.java
@@ -3,12 +3,33 @@ package com.damnervi.practice_02_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView display;
+    private Button b1;
+    private Button b2;
+    private Calculator calculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calculator = new Calculator();
+        display = findViewById(R.id.display);
+        b1 = findViewById(R.id.button1);
+        b2 = findViewById(R.id.button2);
+
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+            display.setText( ((Button) view).getText().toString());
     }
 }
